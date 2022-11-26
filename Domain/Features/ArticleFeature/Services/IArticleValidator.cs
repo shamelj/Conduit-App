93593dd3bf8dde448bf167e8d379fe0df770731d
shamelj <70355802+shamelj@users.noreply.github.com>
2022-11-26 23:51:25 +0200

    Seperated validation logic from article service

---
+++ Domain/Features/ArticleFeature/Services/IArticleValidator.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Features.ArticleFeature.Services;
+
+public interface IArticleValidator
+{
+    Task SlugMustBeUniqueAsync(string slug);
+    Task ArticleMustExistBySlugAsync(string slug);
+    Task UserMustExistAsync(string username);
+}
